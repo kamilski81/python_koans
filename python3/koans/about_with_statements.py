9c1a9e7e8509e5d97537c400768e81a070ca908c
kamil <kamilski81@gmail.com>
2017-11-16 08:41:54 -0600

    45 minutes

--- python3/koans/about_with_statements.py
+++ python3/koans/about_with_statements.py
@@ -42,7 +42,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_finding_lines(self):
-        self.assertEqual('test', self.find_line("example_file.txt"))
+        self.assertEqual("test\n", self.find_line("example_file.txt"))
 
     ## ------------------------------------------------------------------
     ## THINK ABOUT IT:
@@ -100,7 +100,7 @@ class AboutWithStatements(Koan):
                     return line
 
     def test_finding_lines2(self):
-        self.assertEqual('test', self.find_line2("example_file.txt"))
+        self.assertEqual('test\n', self.find_line2("example_file.txt"))
         self.assertNotEqual('huh', self.find_line2("example_file.txt"))
 
     # ------------------------------------------------------------------
